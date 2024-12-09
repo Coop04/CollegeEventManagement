8c6578d232bbfdf779322d3190e28197e014eea4
Coop04 <aswanthsatheesh432@gmail.com>
2024-12-09 16:56:47 +0530

    Initial commit with project files

---
+++ DBmini/users/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import User
+from .models import Event, Registration
+# Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Registration)
+
+# Cooper
+# coopercoop432@gmail.com
+# 4212
